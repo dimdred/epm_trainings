8d49447e3620ba96fd6ba8af63fcc9c35ae9b8d1
dimdred <dimdred@tut.by>
2017-01-10 10:46:12 +0300

    module test

---
+++ string_constants.py
@@ -0,0 +1,14 @@
+import string
+string.ascii_lowercase
+string.digits
+string.ascii_letters
+string.whitespace
+
+import local
+locale.setlocale(locale.LC_ALL, 'ru_RU')
+
+#functions
+string.capwords() #devide by words + add space
+string.maketrans(from, to)#for encription string
+string.atoi() #inactive function
+
